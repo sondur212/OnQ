1f9f8cccc89428309088d03e772309bc4be0765e
Raghavendra Sondur <rmsondur@yahoo.com>
2020-06-29 09:52:48 +0530

    added react-axe

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader';
 const Warning = React.lazy(() => import('./Warning'));
+
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe');
+  axe(React, ReactDOM, 1000);
+}
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -9,9 +15,8 @@ const App = () => {
   const decrement = () => setCount(count - 1);
 
   return (
-    <>
+    <main role="main">
       <h1>Hola New World !!</h1>
-      <img alt="company logo" src="./logo.jpg" />
       <h2 className={count > 5 ? 'warning' : null}>Count: {count}</h2>
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
@@ -20,7 +25,7 @@ const App = () => {
           <Warning />{' '}
         </React.Suspense>
       ) : null}
-    </>
+    </main>
   );
 };
 
